b851dbbed3dce4453229b3adbde68e47a3ec3f3f
thwisse <thwisse@tutanota.com>
2024-09-10 17:08:50 +0300

    fab icine dialog eklendi

--- app/src/main/java/io/github/thwisse/languagedecks/MainActivity.kt
+++ app/src/main/java/io/github/thwisse/languagedecks/MainActivity.kt
@@ -1,20 +1,25 @@
 package io.github.thwisse.languagedecks
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.github.thwisse.languagedecks.databinding.ActivityMainBinding
+import io.github.thwisse.languagedecks.databinding.DialogAddDeckBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapterDecks: AdapterDecks
+    private var deckList = mutableListOf("Turkish - English")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,16 +41,40 @@ class MainActivity : AppCompatActivity() {
         recyclerView = binding.rvDecks
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Test verisi için basit bir liste oluştur
-        val deckList = listOf("Deck 1", "Deck 2", "Deck 3")
-
         // Adapter'i oluştur ve RecyclerView'a bağla
         adapterDecks = AdapterDecks(deckList)
         recyclerView.adapter = adapterDecks
 
         binding.fabAddDeck.setOnClickListener {
-            Toast.makeText(this, "FAB Clicked", Toast.LENGTH_SHORT).show()
+            showAddDeckDialog()
         }
 
+    }
+
+    // Yeni bir deste eklemek için dialog oluşturma
+    private fun showAddDeckDialog() {
+        // DialogAddDeckBinding ile dialog layout'unu bağla
+        val dialogBinding = DialogAddDeckBinding.inflate(LayoutInflater.from(this))
+
+        // AlertDialog oluştur
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Add New Deck")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Add") { dialogInterface, _ ->
+                val deckName = dialogBinding.edtEnterDeckName.text.toString()
+
+                // Deste ismi boş değilse listeye ekle
+                if (deckName.isNotEmpty()) {
+                    deckList.add(deckName)
+                    adapterDecks.notifyDataSetChanged() // RecyclerView'ı güncelle
+                }
+                dialogInterface.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            .create()
+
+        dialog.show()
     }
 }
