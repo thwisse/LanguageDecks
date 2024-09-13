4e3c396f092b99d30576e35b0ea2a2d3f41bbe61
thwisse <thwisse@tutanota.com>
2024-09-13 23:53:36 +0300

    splash screen ve uygulama logosu eklendi

--- app/src/main/java/io/github/thwisse/languagedecks/MainActivity.kt
+++ app/src/main/java/io/github/thwisse/languagedecks/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sharedPreferencesManager: SharedPreferencesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
